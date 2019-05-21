1b987d2f515da2371b761a1df655bf03596e0d31
chenxp38 <1160522425@qq.com>
2019-05-21 15:07:35 +0800

    初步确认注册登录界面，下一步是数据库记录账号密码以供查询验证

---
+++ app/src/main/java/com/example/a11605/dz_project2/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.a11605.dz_project2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/a11605/dz_project2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.a11605.dz_project2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button gotologin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findbyId();
+        gotologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    void findbyId(){
+        gotologin = findViewById(R.id.login_in);
+    }
+}
